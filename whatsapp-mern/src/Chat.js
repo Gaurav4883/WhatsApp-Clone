e6acd8c2c5d25c37c1a726deb74c941d76603bec
gaurav4883 <gaurav.deol1600@gmail.com>
2023-01-27 00:31:32 +0545

    Front-end almost completed

--- whatsapp-mern/src/Chat.js
+++ whatsapp-mern/src/Chat.js
@@ -1,8 +1,8 @@
-import { AttachFile, MoreVert, SearchOutlined } from '@mui/icons-material'
+import { AttachFile, InsertEmoticon, MoreVert, SearchOutlined } from '@mui/icons-material'
 import { Avatar, IconButton } from '@mui/material'
 import React from 'react'
 import './Chat.css'
-
+import MicIcon from '@mui/icons-material/Mic';
 
 function Chat() {
     return (
@@ -32,9 +32,27 @@ function Chat() {
             <div className="chat__body">
                 <p className='chat__message'>
                     <span className='chat__name' >Kamal</span>
-                    This is a message
+                    K cha oe Ishaan ?
                     <span className="chat__timestamp">{new Date().toUTCString()}</span>
                 </p>
+                <p className=' chat__message chat__receiver'>
+                    <span className='chat__name' >Ishaan</span>
+                    Sab thik cha, tero van
+                    <span className="chat__timestamp">{new Date().toUTCString()}</span>
+                </p>
+                <p className='chat__message'>
+                    <span className='chat__name' >Kamal</span>
+                    Yestai ho yaar k hunu
+                    <span className="chat__timestamp">{new Date().toUTCString()}</span>
+                </p>
+            </div>
+            <div className="chat__footer">
+                <InsertEmoticon />
+                <form>
+                    <input type="text" placeholder='Type a Message' />
+                    <button type='submit'>Send</button>
+                </form>
+                <MicIcon />
             </div>
         </div>
     )
